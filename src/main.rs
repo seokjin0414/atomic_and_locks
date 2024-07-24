cdf4b41a596d9242290230d5e2cd292e5e6f26e2
shin <sars21@hanmail.net>
2024-07-24 21:34:04 +0900

    p60

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,28 @@
+use std::{sync::atomic::AtomicUsize, thread, time::Duration};
+use std::sync::atomic::Ordering::Relaxed;
+
 fn main() {
-    println!("Hello, world!");
-    println!("test123");
+    let num_done = AtomicUsize::new(0);
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            for i in 0..100 {
+                process_item(i);
+                num_done.store(i + 1, Relaxed);
+            }
+        });
+
+        loop {
+            let n = num_done.load(Relaxed);
+            if n == 100 {
+                break;
+            }
+            println!("working.. {}/100", n);
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+}
+
+fn process_item(i: usize) {
+    println!("do!!!");
 }
