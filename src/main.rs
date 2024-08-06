0121004a3d5212f10186f279800879d6ec3103b8
shin <sars21@hanmail.net>
2024-08-06 19:05:51 +0900

    clear

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,4 @@ use std::sync::atomic::Ordering::Relaxed;
 use std::time::Instant;
 
 fn main() {
-
-}
-
-fn get_key() -> u64 {
-    static KEY: AtomicU64 = AtomicU64::new(0);
-    let key = KEY.load(Relaxed);
-
-    if key == 0 {
-        let new_key = generate_random_key();
-        match KEY.compare_exchange(0, new_key, Relaxed, Relaxed) {
-            Ok(_) => new_key,
-            Err(k) => k,
-        }
-    } else {
-        key
-    }
 }
