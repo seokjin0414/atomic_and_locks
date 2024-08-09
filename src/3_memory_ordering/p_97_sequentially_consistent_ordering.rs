a64ce1e9497b1d4443f50bc0509527c183ab288d
shin <sars21@hanmail.net>
2024-08-09 21:08:21 +0900

    sequentially consistent ordering

--- src/3_memory_ordering/p_97_sequentially_consistent_ordering.rs
+++ src/3_memory_ordering/p_97_sequentially_consistent_ordering.rs
@@ -1,12 +1,8 @@
 use std::{
-    sync::atomic::{AtomicUsize, AtomicU64, AtomicBool},
+    sync::atomic::{AtomicBool},
     thread,
-    time::Duration
 };
-use std::cmp::max;
-use std::sync::atomic::AtomicPtr;
-use std::sync::atomic::Ordering::{AcqRel, Acquire, Relaxed, Release, SeqCst};
-use std::time::Instant;
+use std::sync::atomic::Ordering::SeqCst;
 
 static A: AtomicBool = AtomicBool::new(false);
 static B: AtomicBool = AtomicBool::new(false);
@@ -15,14 +11,14 @@ static mut S: String = String::new();
 
 fn main() {
     let a = thread::spawn(|| {
-       A.store(true, SeqCst);
+        A.store(true, SeqCst);
         if !B.load(SeqCst) {
             unsafe { S.push('!') };
         }
     });
 
     let b = thread::spawn(|| {
-       B.store (true, SeqCst);
+        B.store (true, SeqCst);
         if !A.load(SeqCst) {
             unsafe { S.push('!') };
         }
