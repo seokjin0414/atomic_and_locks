8b57f4802137db52eb6e5decb3896a9f4d6d7c56
shin <sars21@hanmail.net>
2024-08-14 18:16:58 +0900

    single atomic on channel state 2

---
+++ src/5_channel/p_129_single_atomic_on_channel_state.rs
@@ -0,0 +1,62 @@
+use std::cell::UnsafeCell;
+use std::mem::MaybeUninit;
+use std::sync::atomic::AtomicU8;
+use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
+
+const EMPTY: u8 = 0;
+const WRITING: u8 = 1;
+const READY: u8 = 2;
+const READING: u8 = 3;
+
+pub struct Channel<T> {
+    message: UnsafeCell<MaybeUninit<T>>,
+    state: AtomicU8,
+}
+
+unsafe impl<T: Send> Sync for Channel<T> {}
+
+impl<T> Channel<T> {
+    pub const fn new() -> Self {
+        Self {
+            message: UnsafeCell::new(MaybeUninit::uninit()),
+            state: AtomicU8::new(EMPTY),
+        }
+    }
+
+    pub fn send(&self, message: T) {
+        if self
+            .state
+            .compare_exchange(EMPTY, WRITING, Relaxed, Relaxed)
+            .is_err()
+        {
+            panic!("cant send more than one message!");
+        }
+
+        unsafe { (*self.message.get()).write(message) };
+        self.state.store(READY, Release);
+    }
+
+    pub fn is_ready(&self) -> bool {
+        self.state.load(Relaxed) == READY
+    }
+
+    pub fn receive(&self) -> T {
+        if self
+            .state
+            .compare_exchange(READY, READING, Acquire, Relaxed)
+            .is_err()
+        {
+            panic!("no message available!");
+        }
+
+        unsafe { (*self.message.get()).assume_init_read() }
+    }
+}
+
+impl<T> Drop for Channel<T> {
+    fn drop(&mut self) {
+        if *self.state.get_mut() == READY {
+            unsafe { self.message.get_mut().assume_init_drop() }
+        }
+    }
+}
